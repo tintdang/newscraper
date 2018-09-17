c105f0faf8b7906e3387b6d598325b5361b85058
TinTDang <tin2dang@gmail.com>
2018-09-17 18:21:22 -0500

    added mongoose code

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
+// import the database
+var mongoose = require('mongoose')
+
+// initialize express
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+
+// If deployed, use deployed database. Otherwise use the local newscraper database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newscraper";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 app.engine(
     "handlebars",
