33949cc4cd70f61d678850fc30af41b5cc24ad6c
TDang <tin2dang@gmail.com>
2018-09-17 19:05:17 -0500

    created handlebars files and routing to html pages

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,7 @@
 module.exports = function(app) {
     app.get("/", function (req, res) {
-        res.send("Hello World")
+        res.render("index", {
+            title: "Home"
+        });
     });
 }
